579283ca15287074b7e959a7c33312bb88347c1c
zeedh <zidan.zd49@gmail.com>
2025-05-27 03:38:52 +0700

    Adding inde, show, create, and edit tournament page on dashboard

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::get('dashboard',function(){
 })->middleware('auth');
 
 Route::get('dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
 // Route::get('/', 'App\Http\Controllers\TreeController@index')->name('tree.index');
 // Route::post('/championships/{championship}/trees', 'App\Http\Controllers\TreeController@store')->name('tree.store');
