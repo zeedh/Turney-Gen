71bd3384781ec541cd8aa8b425b9f178d137b61b
zeedh <zidan.zd49@gmail.com>
2025-06-04 21:20:28 +0700

    Styling champs index

--- resources/views/dashboard/champs/index.blade.php
+++ resources/views/dashboard/champs/index.blade.php
@@ -1,9 +1,9 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-4 border-bottom">
-  <h2 class="fw-bold">Bagan Kejuaraan {{ auth()->user()->name }}</h2>
-  <a href="/dashboard/champs/create" class="btn btn-primary">
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h2 class="fw-semibold">Bagan Turnamen {{ auth()->user()->name }}</h2>
+  <a href="/dashboard/tours/create" class="btn btn-primary">
     <i class="bi bi-plus-circle me-1"></i> Buat Bagan Baru
   </a>
 </div>
