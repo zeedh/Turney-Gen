3050817b225865c5db6d3f094b7b8195e90cb78e
zeedh <zidan.zd49@gmail.com>
2025-06-12 07:31:32 +0700

    Styling all the navbar

--- resources/views/dashboard/champs/layouts/header.blade.php
+++ resources/views/dashboard/champs/layouts/header.blade.php
@@ -1,27 +1,34 @@
-<header class="py-3 border-bottom sticky-top bg-white text-dark">
+<header class="py-3 border-bottom sticky-top bg-secondary text-dark">
   <div class="container-fluid d-flex align-items-center justify-content-between" style="grid-template-columns: 1fr 2fr;">
 
     <div>
-      <a href="#" class="align-items-center link-body-emphasis text-decoration-none" aria-expanded="false">
-        TurneyGen
-      </a>
+    <a href="/dashboard" class="text-light text-decoration-none fw-bold fs-5">
+      TurneyGen
+    </a>
     </div>
 
     <div class="dropdown">
-      <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+      <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle text-light" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
       </a>
-      <ul class="dropdown-menu dropdown-menu-end text-small shadow">
-        <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li>
-          <form action="/logout" method="post">
-            @csrf
-            <button type="submit" class="dropdown-item">Logout</button>
-          </form>
-        </li>
-      </ul>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <li>
+                <a class="dropdown-item" href="/dashboard">
+                <i class="bi bi-cast me-1"></i> My Dashboard
+                </a>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li>
+                <form action="/logout" method="post" class="d-inline">
+                @csrf                                
+                  <button type="submit" class="dropdown-item">
+                    <i class="bi bi-box-arrow-right me-1"></i> Logout
+                  </button>
+                </form>
+              </li>
+          </ul>
     </div>
-
   </div>
 </header>
--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<header class="py-3 border-bottom sticky-top bg-danger text-light">
+<header class="py-3 border-bottom sticky-top bg-secondary text-light">
   <div class="container-fluid d-flex align-items-center justify-content-between px-3">
 
     <!-- Sidebar Toggle (Mobile Only) -->
@@ -7,7 +7,7 @@
     </button>
 
     <!-- Brand / Logo -->
-    <a href="/" class="text-light text-decoration-none fw-bold fs-5">
+    <a href="/dashboard" class="text-light text-decoration-none fw-bold fs-5">
       TurneyGen
     </a>
 
@@ -16,17 +16,31 @@
       <a href="#" class="d-flex align-items-center text-light text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" aria-label="User menu">
         <img src="https://github.com/mdo.png" alt="User Avatar" width="32" height="32" class="rounded-circle border border-light">
       </a>
+
       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-light text-small shadow">
-        <li><a class="dropdown-item" href="#">New Post</a></li>
+        <li>
+          <a class="dropdown-item d-flex align-items-center gap-2" href="/blog">
+            <i class="bi bi-house-door-fill text-dark"></i> Halaman Depan
+          </a>
+        </li>
+        <li><hr class="dropdown-divider"></li>
+        <li>
+          <a class="dropdown-item d-flex align-items-center gap-2" href="#">
+            <i class="bi bi-pencil-square text-dark"></i> New Post
+          </a>
+        </li>
         <li><hr class="dropdown-divider"></li>
         <li>
           <form action="/logout" method="post" class="m-0">
             @csrf
-            <button type="submit" class="dropdown-item">Logout</button>
+            <button type="submit" class="dropdown-item d-flex align-items-center gap-2">
+              <i class="bi bi-box-arrow-right text-dark"></i> Logout
+            </button>
           </form>
         </li>
       </ul>
     </div>
+
 
   </div>
 </header>
