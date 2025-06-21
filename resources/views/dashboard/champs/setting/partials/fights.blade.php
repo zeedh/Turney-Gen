dc0c2d263b03f2372aac497a291a6b712f7dae3b
zeedh <zidan.zd49@gmail.com>
2025-06-21 19:39:23 +0700

    Editing fighting list in champs tree

--- resources/views/dashboard/champs/setting/partials/fights.blade.php
+++ resources/views/dashboard/champs/setting/partials/fights.blade.php
@@ -3,16 +3,12 @@
 
     <div class="table-responsive">
         <table class="table table-bordered text-center align-middle">
-
             <thead class="table-light">
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Competitor 1</th>
-                    <th scope="col">Skor 1</th> <!-- Tambahan -->
-                    <th scope="col">Competitor 2</th>
-                    <th scope="col">Skor 2</th> <!-- Tambahan -->
-                    <th scope="col">Pilih Pemenang</th>
-                    <th scope="col">Aksi</th>
+                <tr>    
+                    <th scope="col">Pemain 1</th>
+                    <th scope="col">Skor 1</th>
+                    <th scope="col">Skor 2</th>
+                    <th scope="col">Pemain 2</th>
                 </tr>
             </thead>
             <tbody>
@@ -23,40 +19,25 @@
                             $fighter1 = $fight->fighter1;
                             $fighter2 = $fight->fighter2;
                             $fightId++;
+                            $winnerId = $fight->winner_id;
                         @endphp
                         <tr>
-                            <td>{{ $fightId }}</td>
-                            <td>{{ $fighter1?->fullName ?? 'BYE' }}</td>
-                            <td>{{ $fight->score_c1 ?? '-' }}</td> <!-- Tampilkan skor fighter1 -->
-                            <td>{{ $fighter2?->fullName ?? 'BYE' }}</td>
-                            <td>{{ $fight->score_c2 ?? '-' }}</td> <!-- Tampilkan skor fighter2 -->
-                            <td>
-                                <form action="/dashboard/champs/{{ $championship->id }}/fight/{{ $fight->id }}" method="POST" class="d-flex justify-content-center">
-                                    @csrf
-                                    @method('PUT')
-                                    <select name="winner_id" class="form-select form-select-sm w-auto">
-                                        <option value="">-- Pilih --</option>
-                                        @if($fighter1)
-                                            <option value="{{ $fighter1->id }}" {{ $fight->winner_id == $fighter1->id ? 'selected' : '' }}>
-                                                {{ $fighter1->fullName }}
-                                            </option>
-                                        @endif
-                                        @if($fighter2)
-                                            <option value="{{ $fighter2->id }}" {{ $fight->winner_id == $fighter2->id ? 'selected' : '' }}>
-                                                {{ $fighter2->fullName }}
-                                            </option>
-                                        @endif
-                                    </select>
+                            <td class="{{ $winnerId === $fight->c1 ? 'table-success' : '' }}">
+                                {{ $fighter1?->fullName ?? 'BYE' }}
                             </td>
-                            <td>
-                                    <button type="submit" class="btn btn-sm btn-success">Simpan</button>
-                                </form>
+                            <td class="{{ $winnerId === $fight->c1 ? 'table-success' : '' }}">
+                                {{ $fight->score_c1 ?? '-' }}
+                            </td>
+                            <td class="{{ $winnerId === $fight->c2 ? 'table-success' : '' }}">
+                                {{ $fight->score_c2 ?? '-' }}
+                            </td>
+                            <td class="{{ $winnerId === $fight->c2 ? 'table-success' : '' }}">
+                                {{ $fighter2?->fullName ?? 'BYE' }}
                             </td>
                         </tr>
                     @endif
                 @endforeach
             </tbody>
-
         </table>
     </div>
 @endforeach
