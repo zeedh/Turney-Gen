8bed409834de6a90f6c62b07933448239aa5a288
zeedh <zidan.zd49@gmail.com>
2025-06-22 14:51:16 +0700

    Trying to make a champion list still doesnt work

--- resources/views/dashboard/champs/setting/index.blade.php
+++ resources/views/dashboard/champs/setting/index.blade.php
@@ -11,32 +11,26 @@
     $fights = $championship->fights;
     $numFighters = session('numFighters', $champ->competitors->count());
 
-            $rounds = $championship->fightersGroups->groupBy('round');
-        $finalGroup = $rounds->max();
-        $finalFight = optional($rounds->last())->first()?->fights?->first();
+    $finalRound = $championship->fightersGroups->max('round');
 
-        $winner = $finalFight?->winner;
-        $loser = null;
-        if ($finalFight?->winner_id && $finalFight?->c1 && $finalFight?->c2) {
-            $loser = $finalFight->winner_id == $finalFight->c1 ? $finalFight->c2 : $finalFight->c1;
-        }
+    $finalFight = \Xoco70\LaravelTournaments\Models\Fight::whereHas('group', function ($q) use ($championship, $finalRound) {
+        $q->where('championship_id', $championship->id)
+          ->where('round', $finalRound)
+          ->where('order', 1); // Final biasanya order 1
+    })->first();
 
-        // Ambil semifinal untuk tentukan juara 3
-        $semiGroups = $rounds->get($finalGroup->first()->round - 1) ?? collect();
-        $semiLosers = [];
+    $thirdPlaceFight = \Xoco70\LaravelTournaments\Models\Fight::whereHas('group', function ($q) use ($championship, $finalRound) {
+        $q->where('championship_id', $championship->id)
+          ->where('round', $finalRound)
+          ->where('order', 1)
+          ->latest('id');
+    })->first();
 
-        foreach ($semiGroups as $semi) {
-            $fight = $semi->fights->first();
-            if (!$fight || !$fight->winner_id || !$fight->c1 || !$fight->c2) continue;
-
-            $loserId = $fight->winner_id == $fight->c1 ? $fight->c2 : $fight->c1;
-            if ($loserId != $loser) {
-                $semiLosers[] = $loserId;
-            }
-        }
-
-        $juara3 = \Xoco70\LaravelTournaments\Models\Competitor::find($semiLosers);
-        $juara2 = \Xoco70\LaravelTournaments\Models\Competitor::find($loser);
+    $champion = $finalFight?->winner;
+    $runnerUp = ($finalFight && $finalFight->c1 && $finalFight->c2)
+        ? (($finalFight->winner_id == $finalFight->c1) ? $finalFight->c2Relation : $finalFight->c1Relation)
+        : null;
+    $thirdPlace = $thirdPlaceFight?->winner;
 @endphp
 
 <div class="container-fluid px-3">
@@ -75,28 +69,25 @@
               <table class="table table-bordered text-center mb-0">
                 <thead class="table-light">
                   <tr>
-                    <th>Peringkat</th>
+                    <th>Juara</th>
                     <th>Nama</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td><strong>Juara 1</strong></td>
-                    <td>{{ $winner?->fullName ?? '-' }}</td>
+                    <td>Juara 1</td>
+                    <td>{{ $champion?->fullName ?? '-' }}</td>
                   </tr>
                   <tr>
-                    <td><strong>Juara 2</strong></td>
-                    <td>{{ $juara2?->fullName ?? '-' }}</td>
+                    <td>Juara 2</td>
+                    <td>{{ $runnerUp?->fullName ?? '-' }}</td>
                   </tr>
-                  @foreach($juara3 as $third)
-                    <tr>
-                      <td><strong>Juara 3</strong></td>
-                      <td>{{ $third->fullName ?? '-' }}</td>
-                    </tr>
-                  @endforeach
+                  <tr>
+                    <td>Juara 3</td>
+                    <td>{{ $thirdPlace?->fullName ?? '-' }}</td>
+                  </tr>
                 </tbody>
               </table>
-
           </div>
         </div>
 
