262f0896e7d4d824f713e695497ef56404b076c5
zeedh <zidan.zd49@gmail.com>
2025-07-09 16:57:54 +0700

    Hide all the setting form, use setting from champiosnhip_setting table that already filled when create championship

--- resources/views/dashboard/champs/setting/partials/settings.blade.php
+++ resources/views/dashboard/champs/setting/partials/settings.blade.php
@@ -1,32 +1,30 @@
 <form method="POST" action="{{ route('dashboard.setting.index', ['champ' => $champ->id, 'setting' => $champ->settings->id])}}" accept-charset="UTF-8" class="form-settings">
     @csrf
 
-    <div class="card shadow-sm mb-4">
-        <div class="card-header bg-danger text-white">
-            <h5 class="mb-0">Pengaturan Bagan</h5>
-        </div>
+
         <div class="card-body">
 
             <div class="row g-4">
                 <!-- Preliminary -->
-                <div class="col-lg-4">
+                <!-- <div class="col-lg-4">
                     <label for="hasPreliminary" class="form-label">Preliminary</label>
                     <select class="form-select" id="hasPreliminary" name="hasPreliminary"
                         :disabled="isPrelimDisabled" v-model="hasPrelim" v-on:change="prelim()">
                         <option value="0" {{ $hasPreliminary == 0 ? 'selected' :'' }}>NO</option>
                         <option value="1" {{ $hasPreliminary == 1 ? 'selected' :'' }}>YES</option>
                     </select>
-                </div>
+                </div> -->
+                <input type="hidden" name="hasPreliminary" value="0">
 
                 <!-- Preliminary Group Size -->
-                <div class="col-lg-4">
+                <!-- <div class="col-lg-4">
                     <label for="preliminaryGroupSize" class="form-label">{{ trans('laravel-tournaments::core.preliminaryGroupSize') }}</label>
                     <select class="form-select" id="preliminaryGroupSize" name="preliminaryGroupSize" :disabled="isGroupSizeDisabled">
                         <option value="3" @if ($setting->preliminaryGroupSize == 3) selected @endif>3</option>
                         <option value="4" @if ($setting->preliminaryGroupSize == 4) selected @endif>4</option>
                         <option value="5" @if ($setting->preliminaryGroupSize == 5) selected @endif>5</option>
                     </select>
-                </div>
+                </div> -->
 
                 <!-- Max Fighters -->
                 <!-- <div class="col-lg-4">
@@ -42,25 +40,27 @@
 
 
                 <!-- Team -->
-                <div class="col-lg-4">
+                <!-- <div class="col-lg-4">
                     <label for="isTeam" class="form-label">Team?</label>
                     <select class="form-select" id="isTeam" name="isTeam">
                         <option value="0" {{ $isTeam == 0 ? 'selected' :'' }}>NO</option>
                         <option value="1" {{ $isTeam == 1 ? 'selected' :'' }}>YES</option>
                     </select>
-                </div>
+                </div> -->
+                <input type="hidden" name="isTeam" value="0">
 
                 <!-- Tree Type -->
-                <div class="col-lg-4">
+                <!-- <div class="col-lg-4">
                     <label for="treeType" class="form-label">Tree Type</label>
                     <select class="form-select" id="treeType" name="treeType" v-model="tree" v-on:change="treeType()">
                         <option value="0" @if ($setting->treeType == 0) selected @endif>{{ trans('laravel-tournaments::core.playoff') }}</option>
                         <option value="1" @if ($setting->treeType == 1) selected @endif>{{ trans('laravel-tournaments::core.single_elimination') }}</option>
                     </select>
-                </div>
+                </div> -->
+                <input type="hidden" name="treeType" value="1">
 
                 <!-- Fighting Areas -->
-                <div class="col-lg-4">
+                <!-- <div class="col-lg-4">
                     <label for="fightingAreas" class="form-label">{{ trans_choice('laravel-tournaments::core.fightingArea',2) }}</label>
                     <select class="form-select" id="fightingAreas" name="fightingAreas" :disabled="isAreaDisabled">
                         <option value="1" @if ($setting->fightingAreas == 1) selected @endif>1</option>
@@ -68,7 +68,9 @@
                         <option value="4" @if ($setting->fightingAreas == 4) selected @endif>4</option>
                         <option value="8" @if ($setting->fightingAreas == 8) selected @endif>8</option>
                     </select>
-                </div>
+                </div> -->
+                <input type="hidden" name="fightingAreas" value="{{ $champ->settings->fightingAreas ?? 2 }}">
+
             </div>
 
             <!-- Submit Button -->
@@ -79,5 +81,4 @@
             </div>
 
         </div>
-    </div>
 </form>
